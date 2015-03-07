0cabe54b5177ae6c8af0144b6d1215703dd21b23
niahak <niahak@gmail.com>
2015-03-07 13:11:29 -0500

    Very simple breakout clone with one breakable block

---
+++ SEPlinko/Assets/BreakableBlock.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BreakableBlock : MonoBehaviour {
+
+    /// <summary>
+    /// Number of hits from a game ball to destroy
+    /// </summary>
+    public int hitsToDestroy = 1;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ SEPlinko/Assets/GameBallScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameBallScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    
+    void OnCollisionEnter (Collision col)
+    {
+        var block = col.gameObject.GetComponent<BreakableBlock> ();
+        if (block != null) {
+            block.hitsToDestroy--;
+            if(block.hitsToDestroy == 0)
+            {
+                Destroy (col.gameObject);
+            }
+        }
+
+
+    }
+}
+
