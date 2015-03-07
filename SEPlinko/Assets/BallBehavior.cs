a88d4692f9db38738b9ddb06167d3e491ce61034
niahak <niahak@gmail.com>
2015-03-06 21:21:55 -0500

    Sphere now respawns when it drifts too far.

---
+++ SEPlinko/Assets/BallBehavior.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallBehavior : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+    //Check if the sphere is outside the play area
+	  if (transform.position.z > 25 ||
+            transform.position.y > 25 ||
+            transform.position.y < -25 ||
+            transform.position.x > 25 ||
+            transform.position.x < -25) {
+            Rigidbody rb = GetComponent<Rigidbody>();
+            rb.velocity = new Vector3(0, 0, 0);
+            transform.position = new Vector3(0.0f, 2.49f, 1.89f);
+
+        }
+	}
+}
