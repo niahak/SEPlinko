b8289f7b844bc44eb00fb99bc2a78b4938d4adc2
David Mathew <damathew@sep.com>
2015-03-08 16:12:19 -0400

    Try it out!

--- SEPlinko/Assets/PuckControl.cs
+++ SEPlinko/Assets/PuckControl.cs
@@ -11,11 +11,15 @@ public class PuckControl : MonoBehaviour {
     private Vector3 originalPosition;
     private Quaternion originalRotation;
     private AudioSource source;
+    private bool finishedGame = false;
+    private GameObject winText;
 
     void Awake () {
         source = GetComponent<AudioSource>();
         originalPosition = transform.position;
         originalRotation = transform.rotation;
+        
+        winText = GameObject.Find ("WinText");
     }
 
 	// Use this for initialization
@@ -32,6 +36,9 @@ public class PuckControl : MonoBehaviour {
 
     private void ResetPuck()
     {
+        finishedGame = false;
+        winText.GetComponent<MeshRenderer>().enabled = false;
+
         gameObject.GetComponent<Rigidbody> ().position = originalPosition;
         gameObject.GetComponent<Rigidbody> ().velocity = new Vector3(0,0,0);
 
@@ -46,18 +53,23 @@ public class PuckControl : MonoBehaviour {
         if (col != null) {
             if (col.gameObject.tag == "Wall")
             {
-                source.PlayOneShot(puckDropSound, 1.0f);
+                source.PlayOneShot(pegSound, 1.0f);
                 //transform.position = originalPosition;
             }
             source.PlayOneShot(pegSound, 1.0f);
 
             if (col.gameObject.tag == "PuckBucket")
             {
-                source.PlayOneShot(puckDropSound, 1.0f);
                 bucketCollisions++;
-                if(bucketCollisions >3)
+                if(bucketCollisions >3 && !finishedGame)
                 {
-                    ResetPuck();
+                    finishedGame = true;
+                    winText.GetComponent<MeshRenderer>().enabled = true;
+                    winText.GetComponent<Animator>().Play("Win",-1,0f);
+
+                    source.PlayOneShot(puckDropSound, 1.0f);
+
+                    Invoke ("ResetPuck", 3.0f);
                 }
 
                 //transform.position = originalPosition;
