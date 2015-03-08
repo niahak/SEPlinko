50106f59d27b4fbe6cd56fdb72ef58eb878be351
lrorgan <lrorgan@sep.com>
2015-03-08 10:13:19 -0400

    reset game after puck in cup

--- SEPlinko/Assets/PuckControl.cs
+++ SEPlinko/Assets/PuckControl.cs
@@ -5,6 +5,7 @@ public class PuckControl : MonoBehaviour {
 
     public AudioClip pegSound;
     public AudioClip puckDropSound;
+    int bucketCollisions = 0;
 
     private Vector3 originalPosition;
     private AudioSource source;
@@ -22,19 +23,37 @@ public class PuckControl : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 	    if (gameObject.GetComponent<Rigidbody> ().position.y < 5) {
-            gameObject.GetComponent<Rigidbody> ().position = originalPosition;
-            gameObject.GetComponent<Rigidbody> ().velocity = new Vector3(0,0,0);
+            ResetPuck();
         }
 	}
 
+    private void ResetPuck()
+    {
+        gameObject.GetComponent<Rigidbody> ().position = originalPosition;
+        gameObject.GetComponent<Rigidbody> ().velocity = new Vector3(0,0,0);
+        bucketCollisions = 0;
+    }
+
     void OnCollisionEnter(Collision col) {
         if (col != null) {
-            if (col.gameObject.tag == "PuckBucket")
+            if (col.gameObject.tag == "Wall")
             {
                 source.PlayOneShot(puckDropSound, 1.0f);
                 //transform.position = originalPosition;
             }
             source.PlayOneShot(pegSound, 1.0f);
+
+            if (col.gameObject.tag == "PuckBucket")
+            {
+                source.PlayOneShot(puckDropSound, 1.0f);
+                bucketCollisions++;
+                if(bucketCollisions >3)
+                {
+                    ResetPuck();
+                }
+
+                //transform.position = originalPosition;
+            }
         }
     }
 }
