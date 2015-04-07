fdb8fa688a08975d6b0c84862dec47ece3136fbb
niahak <niahak@gmail.com>
2015-04-07 19:57:56 -0400

    Simple demo using microphone input.

--- 2D/2DTesting/Assets/PlayerMovement.cs
+++ 2D/2DTesting/Assets/PlayerMovement.cs
@@ -6,18 +6,58 @@ public class PlayerMovement : MonoBehaviour {
 
 	private Leap.Controller controller;
 
+	bool _lastEnter = false;
+	bool _lastEsc = false;
+	float highestVolumeSeen = 0f;
 
 	// Use this for initialization
 	void Start () {
 		controller = new Controller ();
 		controller.EnableGesture(Gesture.GestureType.TYPECIRCLE);
 		controller.EnableGesture(Gesture.GestureType.TYPESCREENTAP);
+		highestVolumeSeen = 2;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-		Frame frame = controller.Frame ();
+		var loudness = GetComponent<MicrophoneInput> ().loudness;
+		if (loudness > highestVolumeSeen) {
+			loudness = highestVolumeSeen;
+		}
+
+		var adjustedLoudness = loudness - (highestVolumeSeen / 2);
+
+		var moveSpeed = loudness * 0.08f;
+
+		//Add a "gravity" factor
+		moveSpeed -= 0.040f;
+
+		//Used for debugging...
+		//ScoreManager.score = (int) ( GetComponent<MicrophoneInput> ().loudness * 100);
+
+		if((transform.position.x < -4.5 && moveSpeed < 0) ||
+		   (transform.position.x > 4.5 && moveSpeed > 0))
+		{
+			moveSpeed = 0;
+		}
+		transform.position = new Vector3(transform.position.x + moveSpeed, transform.position.y, transform.position.z);
+
+		if (Input.GetKeyDown (KeyCode.Return) && !_lastEnter) {
+			_lastEnter = true;
+			ScoreManager.CircleAction ();
+		} else if (!Input.GetKeyDown (KeyCode.Return)) {
+			_lastEnter = false;
+		}
+
+		if (Input.GetKeyDown (KeyCode.Escape) && !_lastEsc) {
+			_lastEsc = true;
+			ScoreManager.CircleAction ();
+		} else if (!Input.GetKeyDown (KeyCode.Escape)) {
+			_lastEsc = false;
+		}
+
+		/*Frame frame = controller.Frame ();
 		HandList hands = frame.Hands;
 		Hand leap_hand = hands[0];
 
@@ -42,7 +82,7 @@ public class PlayerMovement : MonoBehaviour {
             if(rb != null)
             {
                 rb.velocity = leap_hand.PalmVelocity.ToUnityScaled() * scaleMovement;
-            }*/
+            }
 			float initialY = gameObject.transform.position.y;
 			gameObject.transform.position = controller_.transform.TransformPoint (leap_hand.PalmPosition.ToUnityScaled ());
 			//Ignore the Z component
@@ -57,6 +97,6 @@ public class PlayerMovement : MonoBehaviour {
 
 			gameObject.transform.position = 
 				new Vector3(newX, initialY, 0);
-		}
+		}*/
 	}
 }
